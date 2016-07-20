a6ddcfbbd8eb5c235d8d4073a54d740f4781731f
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-19 22:25:44 -0400

    Linked up controller, etc. to HTMl and got Like counter working on click

--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -1,1 +1,12 @@
+function Controller ($scope) {
+	
+ 	$scope.counter = 0;
 
+	$scope.likeCounter = function (count) {
+		count = count + 1;
+		$scope.counter = count;
+	}
+
+}
+Controller.$inject = ['$scope'];
+export { Controller };
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 import angular from 'angular';
+
+// Import our Controllers
+import { Controller } from './controllers/controller';
+
+angular
+  .module('app', []) // Register our module (always first)
+  .controller('Controller', Controller);
