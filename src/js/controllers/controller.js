65ab6ad2ba5e949453391e90b56c70a91806fd41
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-19 22:32:01 -0400

    Edited controller.js so that like counter and likes text pluralizes itself correctly

--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -1,12 +1,18 @@
 function Controller ($scope) {
 	
  	$scope.counter = 0;
+ 	$scope.like = "likes";
 
-	$scope.likeCounter = function (count) {
+	$scope.likeCounter = function (count, like) {
 		count = count + 1;
+		like = "likes"
 		$scope.counter = count;
-	}
-
+		if ($scope.counter === 1){
+			$scope.like = "like";
+		} else {
+			$scope.like = "likes";
+		};
+	};
 }
 Controller.$inject = ['$scope'];
 export { Controller };
